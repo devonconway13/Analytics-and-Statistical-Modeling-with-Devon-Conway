ffdec007b5a80184585c657c5a41cfefaa6e44cb
Devon Conway <conway.dev@northeastern.edu>
2021-01-07 11:42:09 -0500

    adding article

---
+++ workspace.R
@@ -0,0 +1,5 @@
+distill::create_blog(dir = "Analytics Blog", title = "Analytics and Statistical Modeling with Devon Conway",
+gh_pages = TRUE, edit = interactive())
+
+#create a theme for blog
+distill::create_theme(name = "theme", edit = TRUE)
